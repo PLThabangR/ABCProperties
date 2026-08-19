2d2fb1dfc4b22777807af0d501ce599cf773e2df
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2026-08-19 02:03:41 +0200

    Creating Wrapper interface and classes for response

---
+++ Application/Wrappers/IResponseWrapper.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace Application.Wrappers
+{
+    public interface IResponseWrapper
+    {
+        public List<string> Messages { get; set; }
+        public bool IsSuccess { get; set; }
+
+
+    }
+
+
+    //Generic inteface 
+    //This will have a response of type T and inherit a none generic one
+    public interface IResponseWrapper<out T> : IResponseWrapper
+    {   
+
+        //propety of type T read
+        public T Data { get; }
+    }
+}
---
+++ Application/Wrappers/ResponseWrapper.cs
@@ -0,0 +1,28 @@
+﻿namespace Application.Wrappers
+{       
+    //This is the implementation of our wrapper interfaces
+    public class ResponseWrapper :IResponseWrapper
+    {
+        public List<string> Messages { get; set; }=[];
+        public bool IsSuccess { get; set; }
+
+        //this will make a fail
+        public static IResponseWrapper Fail(){
+            return new ResponseWrapper (){ IsSuccess=false};
+        }//end of fail m,ethod  
+
+        //if the is a parameter message for failure
+        public static IResponseWrapper Fail(string message){
+            return new ResponseWrapper (){ IsSuccess=false,Messages=[message]};
+        }
+
+        //list of messages 
+        public static IResponseWrapper Fail(List<string> messages){
+            return new ResponseWrapper (){ IsSuccess=false,Messages=messages};
+        }
+        
+
+    }
+
+       
+}
