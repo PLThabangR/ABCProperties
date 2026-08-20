f399ea9d36b6a2f28e5fe71fbcd47a9c8cfbb231
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2026-08-20 13:26:08 +0200

    Complete CQRS for Agnets in the application layer

--- Application/feature/Agents/Queries/GetAgentById.cs
+++ Application/feature/Agents/Queries/GetAgentById.cs
@@ -2,11 +2,7 @@
 using Application.Wrappers;
 using Mapster;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Application.feature.Agents.Queries
 {
---
+++ Application/feature/Agents/Queries/GetAllAgentsQueries.cs
@@ -0,0 +1,39 @@
+﻿using Application.Models.Responds;
+using Application.Wrappers;
+using Mapster;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.feature.Agents.Queries
+{
+    public class GetAllAgentsQueries:IRequest<ResponseWrapper<List<AgentResponse>>>
+    {
+    }
+
+    public class GetAllQueriesHandler : IRequestHandler<GetAllAgentsQueries, ResponseWrapper<List<AgentResponse>>>
+    {
+        private IAgentService agentService;
+
+        public GetAllQueriesHandler(IAgentService agentService)
+        {
+            this.agentService = agentService;
+        }
+
+        public async Task<ResponseWrapper<List<AgentResponse>>> Handle(GetAllAgentsQueries request, CancellationToken cancellationToken)
+        {
+            var agentInDb = await agentService.GetAllAsync();
+
+            if (agentInDb != null) {
+                return ResponseWrapper<List<AgentResponse>>.Fail("No agent were found");
+            }
+            //Convert this to list of agents dtos
+            var agentDTO = agentInDb.Adapt<List<AgentResponse>>();
+
+            return ResponseWrapper<List<AgentResponse>>.Success(data: agentDTO);
+        }
+    }
+}
